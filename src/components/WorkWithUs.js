9a5522f21356f2d979488bc4efed7e191986ef17
TaharkaZ <coolmojo1234@gmail.com>
2021-02-16 11:45:39 -0500

    Added a picture to the work with us page.

--- src/components/WorkWithUs.js
+++ src/components/WorkWithUs.js
@@ -6,6 +6,7 @@ import ServiceGrid from './ServiceGrid';
 import { withRouter, Link, useRouteMatch, NavLink } from 'react-router-dom';
 import Header from './Header';
 import Footer from './Footer';
+import LaptopWoman from '../assets/laptopWoman.png';
 import * as services from '../ServicesOffered.json';
 const ServicesOffered = () => {
     const [data, setData] = useState(services.services);
@@ -17,10 +18,15 @@ const ServicesOffered = () => {
                 <pre class="services-header-text">If your B2B marketing team is on the hook to create more opportunities, faster, Media-Zoo is the answer! 
                 </pre>
             </div>
-            <div class="workwithus-content">
-               <div class="workwithus-block">Learn how our demand generation team can help you execute smarter programs that impact revenue!</div>
-               <div class="workwithus-block">We are a business that thrives on delivering results for our clients. Media-Zoo creates tailored digital marketing strategy with the end goal being driving results, meaningful engagement, and actionable ideas on how to convert lead generation into sales.</div>
-            </div>
+            <div class="workwithus-row">
+                <div class="workwithus-left">
+                    <img src={LaptopWoman} class="workwithus-img"></img>
+                </div>
+                <div class="workwithus-content">
+                <div class="workwithus-block bold">Learn how our demand generation team can help you execute smarter programs that impact revenue!</div>
+                <div class="workwithus-block">We are a business that thrives on delivering results for our clients. Media-Zoo creates tailored digital marketing strategy with the end goal being driving results, meaningful engagement, and actionable ideas on how to convert lead generation into sales.</div>
+                </div>
+            </div> 
             <div class="workwithus-call-to-action">Contact Media-Zoo to get started <a class="btn" href="mailto:contact@media-zoo.com">Contact Us</a></div>
             <Footer></Footer>
         </div>
